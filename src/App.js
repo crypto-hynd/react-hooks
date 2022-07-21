25512040d9763fd27f4ff8d1442b7fccb7d56504
Samee Ullah <samee.ullah@bitflex.com>
2022-07-21 11:12:09 +0500

    Adding bulma.io, creating basic app skelton with bulma and code refactoring

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* TODO */}
+    <div className="App content">
+      <div className={'container'}>
+        <div className={'column is-8 is-offset-2'}>
+          <h1>React Hooks!</h1>
+          {/*  Components will go here */}
+        </div>
+      </div>
     </div>
   );
 }
